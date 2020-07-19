fddf89b74f07e6542b3080e930d5d0c010c966c5
Taotao Pan <pantaotao@icloud.com>
2020-07-19 14:50:45 +1000

    [UPDATE] server file make it more simpler

--- src/server.js
+++ src/server.js
@@ -1,29 +1,37 @@
-import { Server, Model } from "miragejs"
+import { Server } from "miragejs"
 
 export function makeServer({ environment = "test" } = {}) {
     let server = new Server({
         environment,
 
-        models: {
-            trending: Model,
-        },
-
-        seeds(server) {
-            server.create("trending", { tag: "science" })
-            server.create("trending", { tag: "technology" })
-            server.create("trending", { tag: "politics" })
-            server.create("trending", { tag: "sports" })
-            server.create("trending", { tag: "movies" })
-            server.create("trending", { tag: "music" })
-            server.create("trending", { tag: "entertainment" })
-        },
-
         routes() {
             this.namespace = "api"
 
-            this.get("/trendings", (schema) => {
-                return schema.trendings.all()
-            })
+            this.get("/trendings", () => [
+                "science",
+                "technology",
+                "politics",
+                "sports",
+                "movies",
+                "music",
+                "entertainment",
+                "docker",
+                "hibernetes",
+                "ansible",
+                "chef",
+                "git",
+                "puppet",
+                "selenium",
+                "education",
+                "PHP",
+                "history",
+                "life",
+                "stock",
+                "vue",
+                "react",
+                "blockchain",
+                "travel"
+            ])
         },
     })
 
